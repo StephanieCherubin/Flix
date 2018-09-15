a6d881a780a7a1823a3ecd85a2033921591b0af7
Stephanie Cherubin <seraphin.cherubin@gmail.com>
2018-09-15 10:48:07 -0700

    users can see all reviews"

--- app.js
+++ app.js
@@ -5,10 +5,16 @@ var exphbs = require('express-handlebars');
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
+let reviews = [
+  { title: "Great Review" },
+  { title: "Next Review" }
+]
+
 app.get('/', (req, res) => {
-  res.render('home', { msg: 'Hello World!' });
+  res.render('reviews-index', { reviews: reviews });
 })
 
 app.listen(3000, () => {
   console.log('App listening on port 3000!')
 })
+
