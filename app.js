76005b5ae7e4d6fff0c93a8f940619da4cc9fc82
Stephanie Cherubin <seraphin.cherubin@gmail.com>
2018-09-15 11:20:21 -0700

    users can create reviews

--- app.js
+++ app.js
@@ -2,8 +2,9 @@ const express = require('express')
 const app = express()
 const mongoose = require('mongoose');
 const exphbs = require('express-handlebars');
+const bodyParser = require('body-parser');
 
-
+app.use(bodyParser.urlencoded({ extended: true }));
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
@@ -13,6 +14,7 @@ app.set('view engine', 'handlebars');
 //   { title: "Next Review" }
 // ]
 
+// INDEX
 app.get('/', (req, res) => {
   Review.find()
     .then(reviews => {
@@ -21,6 +23,21 @@ app.get('/', (req, res) => {
     .catch(err => {
       console.log(err);
     })
+})
+
+// NEW
+app.get('/reviews/new', (req, res) => {
+  res.render('reviews-new', {});
+})
+
+// CREATE
+app.post('/reviews', (req, res) => {
+  Review.create(req.body).then((review) => {
+    console.log(review);
+    res.redirect('/');
+  }).catch((err) => {
+    console.log(err.message);
+  })
 })
 
 // Mongoose Connection
@@ -32,10 +49,13 @@ mongoose.connect(
 );
 
 const Review = mongoose.model('Review', {
-  title: String
+  title: String,
+  description: String,
+  movieTitle: String
 });
 
 app.listen(3000, () => {
   console.log('App listening on port 3000!')
 })
+
 
