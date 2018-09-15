285a76e2ae16dd8226c1f0c42adeb1bba21d8434
Stephanie Cherubin <seraphin.cherubin@gmail.com>
2018-09-15 11:09:35 -0700

    added mongodb and review model

--- app.js
+++ app.js
@@ -1,18 +1,39 @@
 const express = require('express')
 const app = express()
-var exphbs = require('express-handlebars');
+const mongoose = require('mongoose');
+const exphbs = require('express-handlebars');
+
+
 
 app.engine('handlebars', exphbs({defaultLayout: 'main'}));
 app.set('view engine', 'handlebars');
 
-let reviews = [
-  { title: "Great Review" },
-  { title: "Next Review" }
-]
+// let reviews = [
+//   { title: "Great Review" },
+//   { title: "Next Review" }
+// ]
 
 app.get('/', (req, res) => {
-  res.render('reviews-index', { reviews: reviews });
+  Review.find()
+    .then(reviews => {
+      res.render('reviews-index', { reviews: reviews });
+    })
+    .catch(err => {
+      console.log(err);
+    })
 })
+
+// Mongoose Connection
+const mongoUri =
+   process.env.MONGODB_URI || "mongodb://localhost:27017/RottenPotatoes";
+mongoose.connect(
+   mongoUri,
+   { useNewUrlParser: true }
+);
+
+const Review = mongoose.model('Review', {
+  title: String
+});
 
 app.listen(3000, () => {
   console.log('App listening on port 3000!')
