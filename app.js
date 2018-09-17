82acd469ae483d14d49d098a74030d79f14fbd65
Stephanie Cherubin <seraphin.cherubin@gmail.com>
2018-09-16 21:37:56 -0700

    add root route of movies

--- app.js
+++ app.js
@@ -21,13 +21,7 @@ app.set('view engine', 'handlebars');
 
 // INDEX
 app.get('/', (req, res) => {
-  Review.find()
-    .then(reviews => {
-      res.render('reviews-index', { reviews: reviews });
-    })
-    .catch(err => {
-      console.log(err);
-    })
+  req.render('movies-index');
 })
 
 app.get('/reviews', (req, res) => {
