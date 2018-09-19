424811a04bf67b7564415a1f9ebfb831d827de1f
Stephanie Cherubin <seraphin.cherubin@gmail.com>
2018-09-19 00:23:36 -0700

    users can see overview/summary

--- controllers/movies.js
+++ controllers/movies.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const app = express()
 const MovieDb = require('moviedb-promise')
 const moviedb = new MovieDb('82726f7e5a0d46689161e30a12e353c0')
 const Review = require('../models/review.js');
@@ -14,11 +15,31 @@ app.get('/', (req, res) => {
 })
 
 // SHOW
+
 app.get('/movies/:id', (req, res) => {
     moviedb.movieInfo({ id: req.params.id }).then(movie => {
         console.log(movie)
         res.render('movies-show', { movie: movie });
     }).catch(console.error)
-})
+    })
 }
+
+app.get('/movies/:id', (req, res) => {
+  moviedb.movieInfo({ id: req.params.id }).then(movie => {
+    if (movie.video) {
+      moviedb.movieVideos({ id: req.params.id }).then(videos => {
+        movie.trailer_youtube_id = videos.results[0].key
+        renderTemplate(movie)
+      })
+    } else {
+      renderTemplate(movie)
+    }
+
+    function renderTemplate(movie)  {
+      res.render('movies-show', { movie: movie });
+    }
+
+  }).catch(console.error)
+})
+
 module.exports = movies;
