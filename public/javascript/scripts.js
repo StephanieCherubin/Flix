a1a58ff67391a0a8da69ce445fe7c264b1521f4c
Stephanie Cherubin <seraphin.cherubin@gmail.com>
2018-09-29 11:00:11 -0700

    update the server to respond to json and the script response

--- public/javascript/scripts.js
+++ public/javascript/scripts.js
@@ -33,29 +33,32 @@ document.getElementById("newComment").addEventListener("submit", e => {
     this.reset();
     // display the data as a new comment on the page
     document.getElementById('comments').prepend(
-      `
-       <div class="card">
+        `
+       <div class="card" id="${this._id}">
          <div class="card-block">
            <h4 class="card-title">${response.title}</h4>
            <p class="card-text">${response.content}</p>
-           <button class="btn btn-link" id="deleteComment" data-comment-id=${response._id}>Delete</button>
+           <p><button class="btn btn-link" id="delete-comment" data-comment-id=${response._id}>Delete</button></p>
          </div>
        </div>
       `
     );
   })
 
-  document.querySelector('.delete-comment').addEventListener('click', (e) => {
-    console.log("click!")
-    let commentId = this.getAttribute('data-comment-id')
-    axios.delete(`/reviews/comments/${commentId}`)
-      .then(response => {
-        console.log(response)
-      })
-      .catch(error => {
-        console.log(error)
-      });
-  })
+  document.getElementById('delete-comment').addEventListener('click', (e) => {
+      console.log("click!")
+      let commentId = this.getAttribute('data-comment-id')
+      axios.delete(`/reviews/comments/${commentId}`)
+        .then(response => {
+          console.log(response)
+          comment = document.getElementById(commentId)
+          comment.parentNode.removeChild(comment); // OR comment.style.display = 'none';
+        })
+        .catch(error => {
+          console.log(error)
+          alert('There was an error deleting this comment.')
+        });
+    })
 });
 
 
