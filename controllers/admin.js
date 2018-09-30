1599ef465882654ff5c7892f4d8faf453c625fd9
Stephanie Cherubin <seraphin.cherubin@gmail.com>
2018-09-29 19:10:28 -0700

    admin can see reviews

---
+++ controllers/admin.js
@@ -0,0 +1,16 @@
+const Review = require('./models/review')
+
+module.exports = function (app) {
+
+  // NEW Comment
+  app.get('/admin', (req, res) => {
+    Review.find()
+      .then(reviews => {
+          res.render('admin', { reviews: reviews });
+      })
+      .catch(error => {
+        console.log(error);
+      });
+  });
+
+}
