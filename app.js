8d6d59d2fd34111f87eb85b6f7ae200f4f424b40
Stephanie Cherubin <seraphin.cherubin@gmail.com>
2019-06-16 00:23:07 -0700

    update location for public file

--- app.js
+++ app.js
@@ -15,7 +15,7 @@ const port = process.env.PORT || 3000;
 
 app.use(methodOverride('_method'));
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(express.static('public'));
+// app.use(express.static('public'));
 app.use(bodyParser.json());
 app.use(express.static(__dirname + '/public'));
 
